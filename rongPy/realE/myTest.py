3f0730ba82363ff240cb7fb94b05066d3506d112
I067615 <I067615@PVGN50870758A.apj.global.corp.sap>
2017-09-06 17:32:41 +0800

    re

---
+++ rongPy/realE/myTest.py
@@ -0,0 +1,27 @@
+from realE.reCollector import ReCollector
+from config.configData import configData
+from mail.mailer import Mailer
+import time
+
+config = configData('../config.json')
+
+rc = ReCollector(input('lj account'), input('lj pw'))
+
+xiaoqu_links = rc.get_re_list()
+re_list = rc.get_re_msg(xiaoqu_links)
+
+sender = input('163 account')
+passwd = input('163 pw')
+smtpServer = "smtp.163.com"
+
+mailer = Mailer(sender, passwd, smtpServer)
+
+receiver = ['@gmail.com', '@qq.com']
+subject = "链家每日推送" + time.asctime( time.localtime(time.time()) )
+message = ""
+for msg in re_list:
+    message += (msg + "\n\n")
+
+message = mailer.setMsg(receiver, subject, message)
+
+mailer.send(message)
