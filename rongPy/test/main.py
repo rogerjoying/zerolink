3306c6caa349e8752660c15219a4b9da269a6801
I067615 <I067615@PVGN50870758A.apj.global.corp.sap>
2017-09-14 13:40:09 +0800

    test

--- rongPy/test/main.py
+++ rongPy/test/main.py
@@ -1,3 +1,108 @@
+from operator import attrgetter
+
+class User:
+    
+    def __init__(self, x, y):
+        self.name = x
+        self.user_id = y
+        
+    def __repr__(self):
+        return self.name + " : " + str(self.user_id)
+    
+users = [
+    User('Bucky', 43),
+    User('Sally', 5),
+    User('Tuna', 61),
+    User('Brian', 2),
+    User('Joby', 77),
+    User('Amanda', 9)
+]
+
+for user in users:
+    print(user.__str__())
+
+print('-----')
+for user in sorted(users, key = attrgetter('user_id')):
+    print(user)
+
+'''
+from operator import itemgetter
+
+users = [
+    {'fname': 'Bucky', 'lname' : 'Roberts'},
+    {'fname': 'Tom', 'lname' : 'Roberts'},
+    {'fname': 'Bernie', 'lname' : 'Zunks'},
+    {'fname': 'Jenna', 'lname' : 'Hayes'},
+    {'fname': 'Sally', 'lname' : 'Joness'},
+    {'fname': 'Amanda', 'lname' : 'Roberts'},
+    {'fname': 'Tom', 'lname' : 'Williams'},
+    {'fname': 'Dean', 'lname' : 'Hayes'},
+    {'fname': 'Bernie', 'lname' : 'Barbies'},
+    {'fname': 'Tom', 'lname' : 'Jones'}
+]
+
+for x in sorted(users, key = itemgetter('fname')):
+    print(x)
+
+print('-----')
+
+for x in sorted(users, key = itemgetter('fname', 'lname')):
+    print(x)
+    
+print(sorted(users, key = lambda x: (x['fname'], x['lname'])))
+'''
+
+'''
+from collections import Counter
+
+text = "iPhone X (X pronounced \"ten\" /ˈtɛn/)[1] is a smartphone designed, developed, and marketed by Apple Inc. It was announced on September 12, 2017, alongside the iPhone 8 and iPhone 8 Plus at the Steve Jobs Theater in the Apple Park campus, by Apple CEO Tim Cook, and will be released on November 3, 2017.[2][3][4][5] It is called the iPhone X because it marks the iPhone's tenth anniversary, X being the symbol for \"ten\" in Roman numerals. Within Apple's line-up the X model is positioned as a high-end, premium model intended to showcase advanced technologies. Leaks, including those from case manufacturers, as well as HomePod firmware and the final version of iOS 11, revealed various aspects of the device prior to its unveiling, including that it would have a nearly bezel-less design and no physical home button, an OLED display, dual cameras with improved depth sensing, and a face recognition unlock system known as Face ID.[6][7]"
+
+words = text.split(" ")
+counter = Counter(words)
+top_three = counter.most_common(3)
+print(top_three)
+'''
+
+'''
+from _operator import itemgetter
+
+stocks = {
+        'GOOG' : 434,
+        'AAPL' : 325,
+        'FB' : 54,
+        'AMZN' : 623,
+        'F' : 32,
+        'MSFT' : 549
+}
+
+min_price = min(zip(stocks.values(), stocks.keys()))
+print(min_price)
+
+print(sorted(stocks.items(), key = itemgetter(1)))
+test_min = min(stocks.items(), key = itemgetter(1))
+print(test_min)
+'''
+
+'''
+import heapq
+from _operator import itemgetter
+
+grades = [32, 43, 654, 34, 132, 66, 99, 532]
+
+print(heapq.nlargest(3, grades))
+
+stocks = [
+        {'ticker': 'APPL', 'price': 201},
+        {'ticker': 'GOOG', 'price': 800},
+        {'ticker': 'F', 'price': 54},
+        {'ticker': 'MSFT', 'price': 313},
+        {'ticker': 'TUNA', 'price': 68}
+]
+
+print(sorted(stocks, key = lambda x: x['price']))
+print(heapq.nsmallest(2, stocks, key = lambda x : x['ticker']))
+'''
+
 '''
 from urllib import request
 
--- rongPy/test/pillow.py
+++ rongPy/test/pillow.py
@@ -3,8 +3,6 @@ from PIL import Image
 img = Image.open("./img/zelda.jpg")
 img1 = Image.open("./img/shield.jpg")
 
-print(img.size)
-
 area = (50, 15, 125, 90)
 
 cropped_img = img.crop(area)
@@ -23,4 +21,28 @@ r1, g1, b1 = img1.crop(are2).split()
 new_img = Image.merge("RGB", (b, g, r))
 new_img1 = Image.merge("RGB", (r1, g, b))
 #new_img.show()
-new_img1.show()
+#new_img1.show()
+
+#
+
+img2 = Image.open("./img/mipha.png")
+square_img2 = img2.resize((300, 300))
+flip_img2 = img2.transpose(Image.FLIP_LEFT_RIGHT)
+spin_img2 = img2.transpose(Image.ROTATE_90)
+
+#square_img2.show()
+#flip_img2.show()
+#spin_img2.show()
+
+#
+
+from PIL import ImageFilter
+
+bw_img2 = img2.convert("L")
+blur = img2.filter(ImageFilter.BLUR)
+detail = img2.filter(ImageFilter.DETAIL)
+edges = img2.filter(ImageFilter.FIND_EDGES)
+
+blur.show()
+detail.show()
+edges.show()
